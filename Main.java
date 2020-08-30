aa475c63229526f83e0744be888a3f650b8be770
haneen <57684705+haneen2000@users.noreply.github.com>
2020-08-30 17:34:33 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,204 @@
+package com.company;
+ import java.awt.*;
+ import java.lang.reflect.Array;
+ import java.util.*;
+ import java.util.Scanner;
+ import  java.util.Random;
+ import  java.lang.Math;
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        //1st project
+
+       // System.out.println("hello world");
+        /*
+        System.out.println("hello world");
+        System.out.print("hello world");
+        System.out.print("hello world");
+        */
+        //System.out.println("hello"+" "+"world");
+       // System.out.println("hello\nworld");
+
+
+
+
+        //Variables;
+        /*
+        type name = value;
+        int,short ,long ,float ,double ,string ,character
+         */
+
+        //System.out.println(Integer.MAX_VALUE);
+  /*
+        int x = 44;
+        float y=  0.11f;
+        char z ='$';
+        String a="hello world";
+
+        System.out.println("this is our variable "+x+" "+y+" "+z+a);
+
+        */
+
+
+
+
+        //math operation
+        // + ,_ ,* ,% .
+
+       /* int no1 =20;
+        int no2= 15;
+        int sum=no1+no2;
+        System.out.println(" when we add "+no1+" and "+no2+" the answer is "+sum);
+        System.out.println(no1+no2);
+        System.out.println(no1+" "+no2);
+        */
+       // double d =  Math.sqrt(0.33);
+        //System.out.println(d);
+
+
+
+
+        //I/P from user
+        Scanner input =new Scanner(System.in);
+        /*
+        System.out.println("enter your age please");
+        int age=input.nextInt();
+        System.out.println("enter your name please");
+        String name = input.nextLine();
+        System.out.println(name+ "age is "+ age);
+        */
+
+
+
+
+
+
+        //if
+       /* boolean m=true;
+        if(m){System.out.println("hello world");}
+
+        System.out.println("enter your number please");
+        int no = input.nextInt();
+        */
+    /*
+        if(no==0){System.out.println("zero not even or odd");}
+        else if (no % 2==0){System.out.println("even");}
+        else if (no % 2!=0) {System.out.println("odd");}
+    */
+        /*
+        if(no==0){System.out.println("zero not even or odd");}
+        else if (no%2==0){
+            if(no>50)
+                System.out.println("even and > 50");
+                else{System.out.println("even and < 50");}
+        }
+        else if (no%2!=0){if(no>50)
+            System.out.println("odd and > 50");
+        else{System.out.println("odd and < 50");}
+        }
+        */
+
+        // logical operator
+         //  > , < , <= , >= , == , !() ,!=
+
+
+
+        // switch
+       /*
+        System.out.println("enter your month no please");
+        int no = input.nextInt();
+        switch (no){
+
+            case 0:
+                System.out.println("jan");
+                break;
+            case 1:
+                System.out.println("feb");
+                break;
+            case 2:
+                System.out.println("march");
+                break;
+            case 3:
+                System.out.println("april");
+                break;
+            case 4:
+                System.out.println("may");
+                break;
+            default:
+                System.out.println("month after may");
+        }  */
+
+        // loops
+        //1st for
+        /*
+        for(1;2&5;4){3}
+        */
+      /*  for (int a =0;a<10;a++){
+            System.out.println("hello");
+        }*/
+
+
+        //2nd while
+        /*
+            while (1){
+            2;
+            3;
+            }
+        */
+
+       /* int b=1;
+        while (b<=10){
+            System.out.println("hello");
+            b++;
+        }*/
+
+
+        //3rd do while
+        /*
+         do{
+         1;
+         2;}
+         while(3)
+        */
+    /*
+        int a=10,b=0;
+        do{System.out.println("hello");
+        b++;}while(b<10);
+        System.out.println(b);
+        do{System.out.println("hello");
+            a++;}while(a<10);
+        System.out.println(a);
+        */
+
+
+        //array
+
+    /*   int[] ar =new int[10];
+        for (int a =0;a<ar.length;a++){
+            ar[a] = input.nextInt();}
+
+        for (int a =0;a<ar.length;a++){
+            System.out.println(ar[a]);}
+
+        for (int a =(ar.length-1);a>=0;a--){
+            System.out.println(ar[a]);}
+    */
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
